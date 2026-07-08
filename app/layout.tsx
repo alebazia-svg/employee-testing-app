c676883d471e30dc0456dca472842d490f256f61
Bela Alebazi <alebazia@gmail.com>
2026-07-08 13:28:11 +0300

    fix: prevent mobile web app zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#111821',
 };
 
